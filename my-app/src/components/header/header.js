fd77728c2c8c594996acae125114bbc12ac5cbad
thayorn <thayornswordsman@gmail.com>
2023-08-11 19:14:00 +0300

    create components2

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -1,6 +1,15 @@
 import './header.css'
 
 const Header = () => {
-
+    return (
+        <section className='header-section'>
+            <nav>
+                here will be navbar
+            </nav>
+            <div>
+                and here - header section
+            </div>
+        </section>
+    )
 }
 export default Header
