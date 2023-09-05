5dcadf6e943778748d9cde218be056b1ebe67ab9
thayorn <thayornswordsman@gmail.com>
2023-09-06 01:05:39 +0300

    dropdown trough Menu fragment??

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -5,22 +5,25 @@ import './header.css'
 
 const Header = ({ menuActive,setMenuActive }) => {
     const avatar = <img src={require('../../images/avatar.jpg')} alt='avatar'/>
-    
+    const breadcrumbLinks = [
+        {title: <a href="about">About</a>},
+        {title: <a href="projects">Projects</a>},
+        {title: <a href="contacts">Contacts</a>}
+    ]
 
     return (
         <section className='header-section'>
             <nav className='navbar'>
                 <Avatar size={60} icon={avatar}/>
-
-                <MenuOutlined onClick={() => setMenuActive(!menuActive)}/>
                 
-                <Breadcrumb tabIndex={0}
-                    items = {[
-                        {title: <a href="about">About</a>},
-                        {title: <a href="projects">Projects</a>},
-                        {title: <a href="contacts">Contacts</a>}
-                    ]}
-                />
+                {/* <Dropdown menu={breadcrumbLinks} trigger={['click']}>
+                    
+                        <MenuOutlined onClick={(e) => e.preventDefault()}/>
+                </Dropdown> */}
+                
+                <MenuOutlined onClick={(e) => e.preventDefault()}/>
+                
+                <Breadcrumb tabIndex={0} items = {breadcrumbLinks}/>
             </nav>
             <div className='about-me'>
                 <h1>Hey I'm Valentine</h1>
