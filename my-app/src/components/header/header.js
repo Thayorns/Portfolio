e48533bcb9aa18eb9493c7cbaa9f9c7982340123
Thayorn <thayornSwordsMan@gmail.com>
2023-09-11 20:39:28 +0300

    cards responsive

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -7,9 +7,10 @@ import { useInView } from 'react-intersection-observer'
 import './header.css'
 
 const Header = () => {
-    const { ref, inView, entry } = useInView({threshold: 1,})
+    const { ref, inView, entry } = useInView({threshold: 0.85,})
     const { Meta } = Card;
     const avatar = <img src={require('../../images/avatar.jpg')} alt='avatar'/>
+    
     const breadcrumbLinks = [
         {title: <AnchorLink offset={() => 100} href="#about">About Me</AnchorLink>},
         {title: <AnchorLink offset={() => 100} href="#projects">Projects</AnchorLink>},
@@ -42,34 +43,69 @@ const Header = () => {
             <div className='hobbyes'>
                 By the way, i find myself in an active life position so these are my hobbies:
                 
-                <div className='hobbies-cards' ref={ref} inView={inView}>
-                    <Card hoverable 
-                        cover={
-                            <img alt="my airsoft hobby" 
-                                src={require('../../images/airsoft-hobby.jpg')} 
-                            />
-                        }
-                    >
-                        <Meta title="Airsoft" description="fun military simulator" />
-                    </Card>
-                    
-                    <Card hoverable style={{width: '380px'}}
-                        cover={
-                            <img alt="gaming with my friends" style={{}}
-                                src={require('../../images/gaming.jpg')} 
-                            />
-                        }
-                    >
-                        <Meta title="Gaming" description="especially with my friends" />
-                    </Card>
-                    
-                    <Card hoverable style={{width: '380px'}}
-                        cover={
-                            <img alt="Planes" src={require('../../images/planes.jpg')}/>
-                        }
-                    >
-                        <Meta title="planes" description="learning to fly" />
-                    </Card>
+                <div className='hobbies-cards' ref={ref}>
+                   {/* ------------------------------------------------------------------ */}
+                    <div>
+                        <Card hoverable 
+                            cover={
+                                <img alt="my airsoft hobby" 
+                                    src={require('../../images/airsoft-hobby.jpg')} 
+                                />
+                            }
+                        >
+                            <Meta title="Airsoft" description="fun military simulator" />
+                        </Card>
+                        <Card hoverable 
+                            cover={
+                                <img alt="my airsoft hobby" 
+                                    src={require('../../images/dreamteam.jpg')} 
+                                />
+                            }
+                        >
+                            <Meta title="Jakals & MSOT 4112" description="The most powerful thing in airsoft is to cooperate with others" />
+                        </Card>
+                    </div>
+                    <hr style={{borderColor: "#828585", width: '250px'}}></hr>  
+                    {/* ------------------------------------------------------------------ */}
+                    <div>    
+                        <Card hoverable 
+                            cover={
+                                <img alt="gaming with my friends"
+                                    src={require('../../images/gaming.jpg')} 
+                                />
+                            }
+                        >
+                            <Meta title="Gaming.." description="especially with my friends" />
+                        </Card>
+                        <Card hoverable 
+                            cover={
+                                <img alt="gaming with my friends"
+                                    src={require('../../images/team-screenshot.jpg')} 
+                                />
+                            }
+                        >
+                            <Meta title="'Jakals of Azeroth'" description="By the way, my very 1st pet-project was dedicated to our game-community.The precious moments..." />
+                        </Card>
+                    </div>
+                    {/* ------------------------------------------------------------------ */}
+                    <hr style={{borderColor: "#828585", width: '250px'}}></hr>
+                    <div>    
+                        <Card hoverable 
+                            cover={
+                                <img alt="Planes" src={require('../../images/planes.jpg')}/>
+                            }
+                        >
+                            <Meta title="planes" description="learning to fly" />
+                        </Card>
+
+                        <Card hoverable 
+                            cover={
+                                <img alt="Planes" src={require('../../images/plane.jpg')}/>
+                            }
+                        >
+                            <Meta title="Piper PA" description="This one is my favourite model. It was love at first sight and i don't want any other" />
+                        </Card>
+                    </div>
                 </div>
              </div>
         </section>
