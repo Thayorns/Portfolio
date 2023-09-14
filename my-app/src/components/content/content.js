9b1fe4c0ea968f078a7e1ad113da85cdb521382f
Thayorn <thayornSwordsMan@gmail.com>
2023-09-15 02:18:58 +0300

    projects section cards

--- my-app/src/components/content/content.js
+++ my-app/src/components/content/content.js
@@ -1,10 +1,76 @@
+import { useInView  } from 'react-intersection-observer'
+import { Card } from 'antd';
+
 import './content.css'
 
 const Content = () => {
+    const { Meta } = Card;
+    const { ref: projectsH3TagRef, inView: projectsH3TagInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: jakalsAppRef, inView: jakalsAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: primalsAppRef, inView: primalsAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: calculatorAppRef, inView: calculatorAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: leastCommonMultipleAppRef, inView: leastCommonMultipleAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: fifthAppRef, inView: fifthAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+    const { ref: sixthAppRef, inView: sixthAppInView} = useInView({threshold: 0.3, triggerOnce: true});
+
     return (
-        <section className='content-section'>
-            <div id='projects'>
-                and here - content section
+        <section  id='projects' className='content-section'>
+            <h3 ref={projectsH3TagRef} className={projectsH3TagInView ? 'slide-in' : ''}>Projects:</h3>
+            <div className='projects'>
+                <Card ref={jakalsAppRef} hoverable className={jakalsAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="jakals-app" 
+                            src={require('../../images/jakals.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="Jakals of Azeroth" description="game community app" />
+                </Card>
+                <Card ref={primalsAppRef} hoverable className={primalsAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="find primals app" 
+                            src={require('../../images/primals.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="Primals" description="In mathematics, the sieve of Eratosthenes is an ancient algorithm for finding all prime numbers up to any given limit" />
+                </Card>
+                <Card ref={calculatorAppRef} hoverable className={calculatorAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="calculator-app" 
+                            src={require('../../images/calculator.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="Calculator" description="we all know it is absolutely necessary" />
+                </Card>
+                <Card ref={leastCommonMultipleAppRef} hoverable className={leastCommonMultipleAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="Least-Common-Multiples" 
+                            src={require('../../images/Least-Common-Multiples.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="Least Common Multiples" description="the lowest common multiple of two integers 'a' and 'b', is the smallest positive integer that is divisible by both" />
+                </Card>
+                <Card ref={fifthAppRef} hoverable className={fifthAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="fifth app" 
+                            src={require('../../images/5.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="the fifth something" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nihil" />
+                </Card>
+                <Card ref={sixthAppRef} hoverable className={sixthAppInView ? 'ant-card-hoverable slide-in' : ''}
+                    cover={
+                        <img alt="sixth app" 
+                            src={require('../../images/6.jpg')} 
+                        />
+                    }
+                >
+                    <Meta title="the sixth something" description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis nihil" />
+                </Card>
             </div>
         </section>
     )
--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -28,8 +28,6 @@ const Header = () => {
     const { ref: planesFirstRef, inView: planesFirstInView} = useInView({threshold: 0.3, triggerOnce: true});
     const { ref: planesSecondRef, inView: planesSecondInView} = useInView({threshold: 0.3, triggerOnce: true});
 
-    const { ref: hrTagFirstRef, inView: hrTagFirstInView} = useInView({threshold: 0.4, triggerOnce: true});
-    const { ref: hrTagSecondRef, inView: hrTagSecondInView} = useInView({threshold: 0.4, triggerOnce: true});
 
     return (
         <section className='header-section'>
@@ -43,81 +41,69 @@ const Header = () => {
                 <Breadcrumb tabIndex={0} items = {breadcrumbLinks}/>
             </nav>
             <div className='about-me' id='about'>
-            <h1>Hey! I'm Valentine</h1>
-            <hr style={{borderColor: "#828585", width: '250px'}}></hr>
-            <p>a frontend developer</p>
-            <span>and this is the case when 'coding' === 'my passion' // true</span>
+                <h1>Hey! I'm Valentine</h1>
+                <hr style={{borderColor: "#828585", width: '250px'}}></hr>
+                <p>a frontend developer</p>
+                <span>and this is the case when 'coding' === 'my passion' // true</span>
             </div>
 
-            <div className='hobbyes'>
-                <h3 ref={h3TagRef} className={h3TagInView ? 'slide-in' : ''}>
-                    By the way, i find myself in an active life position so these are my hobbies:
-                </h3>
-                <div className='hobbies-cards'>
-                   {/* ------------------------------------------------------------------ */}
-                   <div>
-                        <Card ref={airsoftFirstRef} hoverable className={airsoftFirstInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="my airsoft hobby" 
-                                    src={require('../../images/airsoft-hobby.jpg')} 
-                                />
-                            }
-                        >
-                            <Meta title="Airsoft" description="fun military simulator" />
-                        </Card>
-                        <Card ref={airsoftSecondRef} hoverable className={airsoftSecondInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="my airsoft hobby"
-                                    src={require('../../images/dreamteam.jpg')} 
-                                />
-                            }
-                        >
-                            <Meta title="Jakals & MSOT 4112" description="The most powerful thing in airsoft is to cooperate with others" />
-                        </Card>
-                    </div>
-                        
-                    <span><hr ref={hrTagFirstRef} className={hrTagFirstInView ? 'slide-in' : ''}></hr></span>  
-                    {/* ------------------------------------------------------------------ */}
-                    <div>    
-                        <Card ref={gamingFirstRef} hoverable className={gamingFirstInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="gaming with my friends"
-                                    src={require('../../images/gaming.jpg')} 
-                                />
-                            }
-                        >
-                            <Meta title="Gaming.." description="especially with my friends" />
-                        </Card>
-                        <Card ref={gamingSecondRef} hoverable className={gamingSecondInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="gaming with my friends"
-                                    src={require('../../images/team-screenshot.jpg')} 
-                                />
-                            }
-                        >
-                            <Meta title="'Jakals of Azeroth'" description="By the way, my very 1st pet-project was dedicated to our game-community.The precious moments..." />
-                        </Card>
-                    </div>
-                    {/* ------------------------------------------------------------------ */}
-                    <hr ref={hrTagSecondRef} className={hrTagSecondInView ? 'slide-in' : ''}></hr>
-                    <div>    
-                        <Card ref={planesFirstRef} hoverable className={planesFirstInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="Planes" src={require('../../images/planes.jpg')}/>
-                            }
-                        >
-                            <Meta title="planes" description="learning to fly" />
-                        </Card>
+            <h3 ref={h3TagRef} className={h3TagInView ? 'slide-in' : ''}>
+                By the way, i find myself in an active life position so these are my hobbies below:
+            </h3>
+                
+            <div className='hobbies-cards'>
+                    <Card ref={airsoftFirstRef} hoverable className={airsoftFirstInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="my airsoft hobby" 
+                                src={require('../../images/airsoft-hobby.jpg')} 
+                            />
+                        }
+                    >
+                        <Meta title="Airsoft" description="fun military simulator" />
+                    </Card>
+                    <Card ref={airsoftSecondRef} hoverable className={airsoftSecondInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="my airsoft hobby"
+                                src={require('../../images/dreamteam.jpg')} 
+                            />
+                        }
+                    >
+                        <Meta title="Jakals & MSOT 4112" description="The most powerful thing in airsoft is to cooperate with others" />
+                    </Card>
+                     
+                    <Card ref={gamingFirstRef} hoverable className={gamingFirstInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="gaming with my friends"
+                                src={require('../../images/gaming.jpg')} 
+                            />
+                        }
+                    >
+                        <Meta title="Gaming.." description="especially with my friends" />
+                    </Card>
+                    <Card ref={gamingSecondRef} hoverable className={gamingSecondInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="gaming with my friends"
+                                src={require('../../images/team-screenshot.jpg')} 
+                            />
+                        }
+                    >
+                        <Meta title="'Jakals of Azeroth'" description="By the way, my very 1st pet-project was dedicated to our game-community.The precious moments..." />
+                    </Card>  
+                    <Card ref={planesFirstRef} hoverable className={planesFirstInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="Planes" src={require('../../images/planes.jpg')}/>
+                        }
+                    >
+                        <Meta title="planes" description="learning to fly" />
+                    </Card>
 
-                        <Card ref={planesSecondRef} hoverable className={planesSecondInView ? 'ant-card-hoverable slide-in' : ''}
-                            cover={
-                                <img alt="Planes" src={require('../../images/plane.jpg')}/>
-                            }
-                        >
-                            <Meta title="Piper PA" description="This one is my favourite model. It was love at first sight and i don't want any other" />
-                        </Card>
-                    </div>
-                </div>
+                    <Card ref={planesSecondRef} hoverable className={planesSecondInView ? 'ant-card-hoverable slide-in' : ''}
+                        cover={
+                            <img alt="Planes" src={require('../../images/plane.jpg')}/>
+                        }
+                    >
+                        <Meta title="Piper PA" description="This one is my favourite model. It was love at first sight and i don't want any other" />
+                    </Card>
             </div>
         </section>
     )
