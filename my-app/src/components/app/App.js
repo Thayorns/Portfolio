35cb8bb4049d89fd4588168f61a096874ed3db73
Thayorn <thayornSwordsMan@gmail.com>
2023-09-17 17:57:11 +0300

    responsive blocks at all pxs

--- my-app/src/components/app/App.js
+++ my-app/src/components/app/App.js
@@ -3,6 +3,9 @@ import Content from '../content/content'
 import Footer from '../footer/footer'
 import Hobbies from '../hobbies/hobbies';
 // import  { useState } from 'react'
+import { FloatButton } from 'antd';
+// import { VerticalAlignTopOutlined } from '@ant-design/icons';
+
 
 import './App.css';
 
@@ -15,6 +18,7 @@ const App = () => {
       <Content/>
       <Hobbies/>
       <Footer/>
+      <FloatButton.BackTop/>
     </div>
   )
 }
