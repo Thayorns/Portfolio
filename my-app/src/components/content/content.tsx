6e0a9f6da8da7e046ba4b6fc00598982697a1e5e
valentine <thayornswordsman@gmail.com>
2025-02-27 06:20:06 +0300

    new projects added

--- my-app/src/components/content/content.tsx
+++ my-app/src/components/content/content.tsx
@@ -20,13 +20,13 @@ interface PetProjects {
     link: string
 }
 const projects = [
-    
-    {src: require('../../images/games-query.jpg'), title: 'Игровая библиотека', description: 'React SPA с использованием RTK query, FSD методологии && react router dom 6, более расширенная версия с большим функционалом.',link: 'https://games-query.netlify.app/'},
-    {src: require('../../images/rtk-query.png'), title: 'RTK query', description: 'React SPA с использованием RTK query, FSD методологии && react router dom 6.',link: 'https://rtkquery-fsd-rrd6.netlify.app/'},
-    {src: require('../../images/smallLogo.png'), title: 'Black Bears brewery', description: 'Это мой первый фриланс-проект. Никогда не бывает много пива..',link: 'https://black-bears-brewery.com/'},
-    {src: require('../../images/primals.jpg'), title: 'Простые числа', description: 'SPA приложение показывает, является ли введённое в поле число/цифра - простым. ',link: 'https://primals.netlify.app/'},
-    {src: require('../../images/fetch-data.png'), title: 'Fetch данных', description: "Простое SPA с моей первой fetch-функцией!",link: 'https://fetch-data-onclick.netlify.app/'},
-    {src: require('../../images/jakals.jpg'), title: 'Шакалы Азерота', description: 'Это самый первый проект, посвящённый моим друзьям в игре World of Warcraft.',link: 'https://jakals-of-azeroth.netlify.app/'},
+    {src: require('../../images/creamkorzh.png'), title: 'КРЕМ и КОРЖ', description: 'Приложение для пекарни-кондитерской. Учёт подарочных кофе по qr-коду, регистрация/авторизация по почте, уведомления и корзина для заказов тортов.', link: 'https://creamkorzh.ru/'},
+    {src: require('../../images/games-query.jpg'), title: 'Игровая библиотека', description: 'React приложение получает данные по внешнему API, рендерит в таблицу с возможностью фильтра по категориям. ограничение - 50 игр.',link: 'https://games-query.netlify.app/'},
+    {src: require('../../images/rtk-query.png'), title: 'Запросы к API', description: 'React приложение получает данные из jsonplaceholder с подгрузкой следующей партии данных по клику. Реализован роутинг с router dom 6.',link: 'https://rtkquery-fsd-rrd6.netlify.app/'},
+    {src: require('../../images/smallLogo.png'), title: 'Black Bears brewery', description: 'Визитная карточка пивоварни "Black Bears Brewery". Много пива не бывает..',link: 'https://black-bears-brewery.netlify.app/'},
+    {src: require('../../images/primals.jpg'), title: 'Простые числа', description: 'Небольшое реакт-приложение. Показывает, является ли введённое в поле число/цифра простым.',link: 'https://primals.netlify.app/'},
+    // {src: require('../../images/fetch-data.png'), title: 'Fetch данных', description: "Простое SPA с моей первой fetch-функцией!",link: 'https://fetch-data-onclick.netlify.app/'},
+    // {src: require('../../images/jakals.jpg'), title: 'Шакалы Азерота', description: 'Это самый первый проект, посвящённый моим друзьям в игре World of Warcraft.',link: 'https://jakals-of-azeroth.netlify.app/'},
 ]
 
 const Content = () => {
